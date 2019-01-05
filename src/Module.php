17a88894fbd0766fe2c7531fbc658322e4bce714
Devils <devortix@gmail.com>
2019-01-05 11:09:02 +0200

    init

---
+++ src/Module.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace devortix\user;
+
+/**
+ * main module definition class
+ */
+class Module extends \dektrium\user\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'dektrium\user\controllers';
+    public $controllerMap = [
+        'api' => 'devortix\user\controllers',
+    ];
+    public $viewPath = '@vendor/dektrium/yii2-user/views';
+    // public $layoutPath = '@vendor/dektrium/yii2-user/views'
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        $this->setViewPath('@vendor/dektrium/yii2-user/views');
+        parent::init();
+
+    }
+}
---
+++ src/controllers/ApiController.php
@@ -0,0 +1,74 @@
+<?php
+namespace devortix\user\controllers;
+
+use dektrium\user\models\LoginForm;
+use yii\filters\auth\HttpBearerAuth;
+use yii\rest\ActiveController;
+use yii\web\Response;
+
+class ApiController extends ActiveController
+{
+    public function beforeAction($action)
+    {
+        \Yii::$app->user->enableSession = false;
+
+        return parent::beforeAction($action);
+    }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+        ];
+        $behaviors['contentNegotiator'] = [
+            'class' => \yii\filters\ContentNegotiator::className(),
+            'formats' => [
+                'application/json' => \yii\web\Response::FORMAT_JSON,
+            ],
+        ];
+        unset($behaviors['authenticator']);
+        $behaviors['authenticator']['class'] = HttpBearerAuth::className();
+        $behaviors['authenticator']['except'] = ['login'];
+        return $behaviors;
+    }
+
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'items',
+    ];
+
+    public function actionLogin()
+    {
+        $model = \Yii::createObject(LoginForm::className());
+        $username = \Yii::$app->request->post('username');
+        $password = \Yii::$app->request->post('password');
+        $model->login = $username;
+        $model->password = $password;
+
+        if ($model->login()) {
+
+            return [
+                'token' => \Yii::$app->getUser()->identity->auth_key,
+            ];
+        }
+
+        throw new \yii\web\HttpException(401, 'User not found!');
+    }
+
+    public function actionToken()
+    {
+
+        if (\Yii::$app->getUser()) {
+            return [
+                'user' => [
+                    'username' => \Yii::$app->getUser()->identity->username,
+                    'token' => \Yii::$app->getUser()->identity->auth_key,
+                ],
+            ];
+        }
+
+    }
+
+}
