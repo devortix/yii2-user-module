829c69a607fe8923a0e9b36082b62563dbf4e29f
Devils <devortix@gmail.com>
2019-01-22 20:52:33 +0200

    added user model

---
+++ src/models/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace devortix\models;
+
+use dektrium\user\models\User as UserBase;
+
+class User extends UserBase implements \yii\web\IdentityInterface
+{
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['auth_key' => $token]);
+    }
+}
